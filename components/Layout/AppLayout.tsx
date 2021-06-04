4d94ff3fc4912b74b77ab6844b8ddd58c086ed35
Jonathan Dorety <jdorety@flur.ee>
2021-06-03 23:55:44 -0400

    Add some padding, alignment

--- components/Layout/AppLayout.tsx
+++ components/Layout/AppLayout.tsx
@@ -9,7 +9,9 @@ export default function AppLayout({ children }: AppLayoutProps) {
   return (
     <>
       <TopBar />
+      <div className="container mx-auto px-8 pt-4">
       {children}
+      </div>
     </>
   );
 }
