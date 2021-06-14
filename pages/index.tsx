f69735caf1c803ab6f9fb103463c8317a037ae76
Jonathan Dorety <jdorety@flur.ee>
2021-06-13 20:59:33 -0400

    Set up icons, manifest, main page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <h2 className="text-4xl font-bold">Posts</h2>
-  )
+    <>
+      <Head>
+        <title>Oh Christ, Another X-Men Blog</title>
+      </Head>
+      <h2 className="text-4xl font-bold">Posts</h2>
+    </>
+  );
 }
